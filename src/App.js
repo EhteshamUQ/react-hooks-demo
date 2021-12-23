a279e0e87672b90b97f8de5901c022e498684353
Ehtesham <ehteshamuddinqureshi.mohd@zemosolabs.com>
2021-12-23 15:07:11 +0530

    Using useEffect and useState Hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "./components/atoms/Button";
+import { React, useState, useEffect } from "react";
 
 function App() {
+  const [resourceType, setResourceType] = useState("posts");
+  const onClick = (e) => {
+    let title = e.target.innerHTML;
+    // console.log(title);
+    setResourceType(title);
+  };
+
+  // useEffect(() => {
+  //   console.log("Called Everry time when it renders");
+  // });
+
+  useEffect(() => {
+    console.log(
+      "Called only when Mouting an Element , i.e ComponentDidMount Functionality"
+    );
+    
+  }, []);
+
+  useEffect(() => {
+    console.log("Changed resourceType");
+    return () => {
+      console.log("Clean-Up Called - This Method can be used for DidUnMount");
+    }
+  }, [resourceType]);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="buttonList">
+        <Button onClick={onClick} title="posts" />
+        <Button onClick={onClick} title="users" />
+        <Button onClick={onClick} title="comments" />
+      </div>
+      <h1>{resourceType}</h1>
+    </>
   );
 }
 
---
+++ src/components/atoms/Button.jsx
@@ -0,0 +1,5 @@
+const Button = ({ title, id , onClick }) => {
+  return <button id={id} onClick = {onClick}>{title}</button>;
+};
+
+export default Button;
